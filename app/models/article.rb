0a178bea8129ec15b97d8c38527f068eb039cebc
Kayzredman <Kayzredman@gmail.com>
2020-02-22 14:27:11 +0000

    After being able to save to DB_show form yet to create

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximumu: 50 }
-  validates :description, true, length: { minimum: 10, maximumu: 300 }
+  validates :description, presence: true, length: { minimum: 10, maximumu: 300 }
 end
