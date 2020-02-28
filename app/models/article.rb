04c3ab1caed5bca6d48237564452ce2710a1c363
Kayzredman <Kayzredman@gmail.com>
2020-02-28 12:08:04 +0000

    After adding assoc btwn user_n_aritcles with dbgr

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximumu: 50 }
   validates :description, presence: true, length: { minimum: 10, maximumu: 300 }
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_191227) do
+ActiveRecord::Schema.define(version: 2020_02_27_225514) do
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
