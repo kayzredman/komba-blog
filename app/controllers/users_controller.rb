c788856a71569f11475c72f68de7fc10c88b7a37
Kayzredman <Kayzredman@gmail.com>
2020-03-04 23:49:42 +0000

    Add admin_user delete function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
-
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
       def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -30,6 +30,13 @@ class UsersController < ApplicationController
           @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
       end
 
+      def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "User and all articles created will be deleted"
+        redirect_to users_path
+      end
+
       def update
           if @user.update(user_params)
           flash[:success] = "Your User account was successfully Updated"
@@ -49,8 +56,15 @@ class UsersController < ApplicationController
       end
 
       def require_same_user
-          if current_user != @user
+          if current_user != @user and !current_user.admin?
             flash[:danger] = "You can only edit you own account"
+            redirect_to root_path
+          end
+      end
+
+      def require_admin
+          if logged_in? and !current_user.admin?
+            flash[:danger] = "Only Admin Users can perform that action"
             redirect_to root_path
           end
       end
