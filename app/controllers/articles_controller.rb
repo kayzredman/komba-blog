d0749a577d0d83227635c98ebfb1891ce709da81
Kayzredman <Kayzredman@gmail.com>
2020-03-04 22:48:32 +0000

    Add admin_user function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-  
+
     end
 
     def create
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
       end
 
       def require_same_user
-          if current_user != @article.user
+          if current_user != @article.user and !current_user.admin?
             flash[:danger] = "Blimey..! You can only edit or delete your own articles"
             redirect_to root_path
           end
