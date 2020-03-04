a0a3c035509f365c53dbbee5b25f50bc902a8d2c
Kayzredman <Kayzredman@gmail.com>
2020-03-04 20:43:56 +0000

    Complete Auth system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,56 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
 
 
-    def index
-      @users = User.paginate(page: params[:page], per_page: 5)
-    end
+      def index
+        @users = User.paginate(page: params[:page], per_page: 5)
+      end
 
-    def new
-      @user = User.new
-    end
+      def new
+        @user = User.new
+      end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          flash[:success] = "Welcome to the Komba Blog #{@user.username}"
+      def create
+          @user = User.new(user_params)
+          if @user.save
+            flash[:success] = "Welcome to the Komba Blog #{@user.username}"
+            redirect_to articles_path
+          else
+              render 'new'
+          end
+      end
+
+      def edit
+
+      end
+
+      def show
+          @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+      end
+
+      def update
+          if @user.update(user_params)
+          flash[:success] = "Your User account was successfully Updated"
           redirect_to articles_path
-        else
-            render 'new'
-        end
-    end
+          else
+            render 'edit'
+          end
+      end
 
-    def edit
+      private
+      def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
 
-    end
+      def set_user
+        @user = User.find(params[:id])
+      end
 
-    def show
-        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-    end
-
-    def update
-        if @user.update(user_params)
-        flash[:success] = "Your User account was successfully Updated"
-        redirect_to articles_path
-        else
-          render 'edit'
-        end
-    end
-
-    private
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
-
-    def set_user
-      @user = User.find(params[:id])
-    end
-
+      def require_same_user
+          if current_user != @user
+            flash[:danger] = "You can only edit you own account"
+            redirect_to root_path
+          end
+      end
 end
