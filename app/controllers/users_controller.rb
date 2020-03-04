f4d1bd2c8da69a96f044332e2c2ab7dcce8cfd22
Kayzredman <Kayzredman@gmail.com>
2020-03-04 16:48:24 +0000

    after authentication methods setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,49 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 
-  def index
-    @users = User.paginate(page: params[:page], per_page: 5)
-  end
 
-  def new
-    @user = User.new
-  end
+    def index
+      @users = User.paginate(page: params[:page], per_page: 5)
+    end
 
-  def create
-      @user = User.new(user_params)
-      if @user.save
-        flash[:success] = "Welcome to the Komba Blog #{@user.username}"
+    def new
+      @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          flash[:success] = "Welcome to the Komba Blog #{@user.username}"
+          redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+
+    end
+
+    def show
+        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+
+    def update
+        if @user.update(user_params)
+        flash[:success] = "Your User account was successfully Updated"
         redirect_to articles_path
-      else
-          render 'new'
-      end
-  end
+        else
+          render 'edit'
+        end
+    end
 
-  def edit
+    private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
       @user = User.find(params[:id])
-  end
-
-  def show
-      @user = User.find(params[:id])
-      @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-  end
-
-  def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-      flash[:success] = "Your User account was successfully Updated"
-      redirect_to articles_path
-      else
-        render 'edit'
-      end
-  end
-
-
-
-  private
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+    end
 
 end
