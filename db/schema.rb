3d4d09068007ca21a944f75a5511e6c8533e3286
Kayzredman <Kayzredman@gmail.com>
2020-03-08 19:08:55 +0000

    After Build of Article_category assoc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_110535) do
+ActiveRecord::Schema.define(version: 2020_03_07_213200) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.text "title"
